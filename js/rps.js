21cfc515b5d2430351cda873c32c2410731f119b
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-05-24 21:48:51 -0600

    final version. Website is responsive

--- js/rps.js
+++ js/rps.js
@@ -7,25 +7,29 @@ const computerWeaponImg = document.querySelector("#computerSelection img");
 const playerScore = document.querySelector("#playerScore").childNodes[0];
 const computerScore = document.querySelector("#computerScore").childNodes[0];
 const weaponButtons = document.querySelectorAll(".weaponButton");
-
-const noDisplay = document.querySelector("#container").style;
-//noDisplay.display = "none";
+const gameResult = document.querySelector("#gameResultH2");
+const noDisplay1 = document.querySelector("#gameContainer").style;
+const noDisplay2 = document.querySelector("#resultsContainer").style;
+const playAgain = document.querySelector("#playAgain");
+const playAgainBtn = document.querySelector("#playAgainBtn");
+const summaryData = document.querySelector("#summaryData");
+noDisplay2.display = "none";
 
 weaponButtons.forEach((button) => {
     button.addEventListener("click", _ => {
+        button.classList.add("weaponClick");
+        transitionClick(button);
         playerPlay(button);
         game(playerPlay(button));
     });
 });  
 
-/*if (playerScore.textContent == 5) {
-    let gameResult = "You won!";
-    window.location.href = 'gameresult.html'; 
-}
-else if (computerScore.textContent == 5) {
-    let gameResult = "You lost!\nTry again!";
-    window.location.href = 'gameresult.html'; 
-}*/
+function transitionClick(button) {
+    button.addEventListener("transitionend", (e) => {
+    if (e.propertyName !== 'transform') return;
+    button.classList.remove("weaponClick");
+    });
+  }
 
 function playerPlay(button) {
     return button.getAttribute("id");
@@ -53,6 +57,8 @@ function game(playerPlay) {
     
     roundFeedback.textContent = playRound(playerSelection, computerSelection);
 
+    logRound(playerSelection, computerSelection, roundFeedback.textContent, roundNumber.textContent);
+    if (playerScore.textContent == 5 || computerScore.textContent == 5) loadResults(roundFeedback.textContent);
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -62,14 +68,14 @@ function playRound(playerSelection, computerSelection) {
         case "paperrock":   
         case "scissorspaper":
             roundResult = "You Win!";
-            playerScore.textContent = `${playerScore.textContent * 1 + 1}`;
+            playerScore.textContent = `${+playerScore.textContent + 1}`;
             break;
             
         case "scissorsrock":
         case "rockpaper":
         case "paperscissors":
             roundResult = "You Lose!";
-            computerScore.textContent = `${computerScore.textContent * 1 + 1}`;
+            computerScore.textContent = `${+computerScore.textContent + 1}`;
             break;
 
         default:
@@ -78,20 +84,95 @@ function playRound(playerSelection, computerSelection) {
     return roundResult;
 }
 
-/*function gameScore() {
-    if (roundResult == "You Win!") {
-        roundScoreHuman += 1;
-        playerScore.textContent = roundScoreHuman;
-        if (playerScore.textContent == 5) {
-            let gameResult = "You won!";
-            window.location.href = 'gameresult.html'; 
-        }      
-    }
-    else if (roundResult == "You Lose!") {
-        roundScoreComputer += 1;
-        computerScore.textContent = roundScoreComputer;
+function loadResults(winner){
+    noDisplay1.display = "none";
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+    roundNumber.textContent = "Round";
+    i = 0;
+    roundResult = "";
+    const trophyAttribution = document.querySelector(".imgAttribution2");
+    if (winner == "You Win!") {
+        const trophyDiv = document.createElement("div");
+        trophyDiv.setAttribute("id", "trophyDiv");
+        const trophyImg = document.createElement("img");
+        trophyImg.setAttribute("id", "trophyImg");
+        trophyImg.setAttribute("src", "images/trophy.png");
+        trophyDiv.appendChild(trophyImg);
+        const gameResultDiv = document.querySelector("#gameResultDiv");
+        gameResultDiv.insertBefore(trophyDiv, playAgain);
+        trophyAttribution.style.display ="block";
+        gameResult.textContent = "You Won!";
     }
     else {
-        return;
+        trophyAttribution.style.display ="none";
+        gameResult.textContent = `You Lost! Try Again!`;
     }
-}*/
+    noDisplay2.display = "block";
+    weaponButtons.forEach((button) => {
+        button.classList.remove("weaponClick");
+    });
+}
+
+playAgainBtn.addEventListener("click", _ => {
+    const playerRound = document.querySelectorAll(".playerRound");
+    noDisplay2.display = "none";
+    playerRound.forEach((childNode) => {
+        summaryData.removeChild(childNode);
+    });
+    if (gameResult.textContent == "You Won!" && trophyDiv) gameResultDiv.removeChild(trophyDiv);
+    playerWeaponImg.setAttribute("src", "images/480px-Question_mark_white_icon.svg.png");
+    computerWeaponImg.setAttribute("src", "images/480px-Question_mark_white_icon.svg.png");
+    roundFeedback.textContent = "";
+    noDisplay1.display = "block";
+});
+
+function logRound(playerSelection, computerSelection, roundFeedback, roundCounter) {
+    const newRoundLog = document.createElement("div");
+    newRoundLog.setAttribute("class", "playerRound");
+    const summaryPlayer = document.createElement("div");
+    summaryPlayer.setAttribute("class", "summaryPlayer");
+    newRoundLog.appendChild(summaryPlayer);
+    const roundNumberLog = document.createElement("h2");
+    roundNumberLog.setAttribute("class", "roundNumber");
+    roundNumberLog.textContent = roundCounter.slice(6);
+    newRoundLog.appendChild(roundNumberLog);
+    const summaryComputer = document.createElement("div");
+    summaryComputer.setAttribute("class", "summaryComputer");
+    newRoundLog.appendChild(summaryComputer);
+    const roundImgContainerPlayer = document.createElement("div");
+    roundImgContainerPlayer.setAttribute("class", "roundImgContainer");
+    summaryPlayer.appendChild(roundImgContainerPlayer);
+    const roundImgContainerComputer = document.createElement("div");
+    roundImgContainerComputer.setAttribute("class", "roundImgContainer");
+    summaryComputer.appendChild(roundImgContainerComputer);
+    const roundImgPlayer = document.createElement("img");
+    roundImgPlayer.setAttribute("class", "roundImgPlayer");
+    roundImgPlayer.setAttribute("src", `${document.querySelector(`#${playerSelection} img`).getAttribute("src")}`);
+    roundImgContainerPlayer.appendChild(roundImgPlayer);
+    const roundImgComputer = document.createElement("img");
+    roundImgComputer.setAttribute("class", "roundImgComputer");
+    roundImgComputer.setAttribute("src", `${document.querySelector(`#${computerSelection} img`).getAttribute("src")}`);
+    roundImgContainerComputer.appendChild(roundImgComputer);
+    const winnerMarkPlayer = document.createElement("img");
+    winnerMarkPlayer.setAttribute("class", "winnerMarkPlayer");
+    summaryPlayer.appendChild(winnerMarkPlayer);
+    const winnerMarkComputer = document.createElement("img");
+    winnerMarkComputer.setAttribute("class", "winnerMarkComputer");
+    summaryComputer.appendChild(winnerMarkComputer);
+    if (roundFeedback == "You Win!") {
+        winnerMarkPlayer.setAttribute("src", "images/checkmark-png-25969.png");
+        winnerMarkComputer.setAttribute("src", "images/Red-Cross-Mark-PNG-File.png");
+    }
+    else if (roundFeedback == "You Lose!") {
+        winnerMarkPlayer.setAttribute("src", "images/Red-Cross-Mark-PNG-File.png");
+        winnerMarkComputer.setAttribute("src", "images/checkmark-png-25969.png");
+    }
+    else {
+        winnerMarkPlayer.setAttribute("src", "");
+        winnerMarkComputer.setAttribute("src", "");
+    }
+    summaryData.appendChild(newRoundLog);
+}
+
+
